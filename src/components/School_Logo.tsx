be138c8e1e5f5cfea056e30485e35f2e2bef6fbe
eduemeweb <srichakra.b.2004@gmail.com>
2024-10-29 23:44:56 +0530

    Resize of Logos

--- src/components/School_Logo.tsx
+++ src/components/School_Logo.tsx
@@ -4,11 +4,11 @@ import React from 'react'
 const School_Logo = () => {
   return (
     <div className='flex items-center justify-between px-[10%] pt-[1rem]'>
-        <img src='/School1.png' alt='Samskar The Life School' ></img>
-        <img src='/School2.png' alt='Akar International School'></img>
-        <img src='/School3.png' alt='SriVeda The Universe School'></img>
-        <img src='/School4.png' alt='Mount Caramel Global School'></img>
-        <img src='/School5.png' alt='Samartha School'></img>
+        <img src='/School1.png' className='w-[170px] h-[170px]' alt='Samskar The Life School' ></img>
+        <img src='/School2.png' className='w-[170px] h-[170px]' alt='Akar International School'></img>
+        <img src='/School3.png' className='w-[170px] h-[170px]' alt='SriVeda The Universe School'></img>
+        <img src='/School4.png' className='w-[170px] h-[170px]' alt='Mount Caramel Global School'></img>
+        <img src='/School5.png' className='w-[170px] h-[170px]'alt='Samartha School'></img>
     </div>
   )
 }
