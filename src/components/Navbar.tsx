f1af90996141dc6bf471849dbc2741733ce5c623
eduemeweb <edumeintern@gmail.com>
2024-10-29 16:06:11 +0530

    Style added to buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,11 +9,11 @@ const Navbar = () => {
         </div>
 
         <div className='flex gap-[1rem] items-center'>
-            <Link href="/">Home</Link>
-            <Link href="/about" className='hover:bg-yellow-500 hover:text-white'>About</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href={"/test"}>Test</Link>
+            <Link href="/" className='hover:bg-yellow-500 hover:text-white px-[1rem] py-[0.5rem] rounded-md'>Home</Link>
+            <Link href="/about" className='hover:bg-yellow-500 hover:text-white px-[1rem] py-[0.5rem] rounded-md'>About</Link>
+            <Link href="/contact" className='hover:bg-yellow-500 hover:text-white px-[1rem] py-[0.5rem] rounded-md'>Contact</Link>
+            <Link href="/blog"className='hover:bg-yellow-500 hover:text-white px-[1rem] py-[0.5rem] rounded-md'>Blog</Link>
+            <Link href={"/test"}className='hover:bg-yellow-500 hover:text-white px-[1rem] py-[0.5rem] rounded-md'>Test</Link>
             <Link href={"/shop"} className='bg-blue-600 text-white px-[1rem] py-[0.5rem] rounded-md'>Shop Now</Link>
         </div>
     </div>
