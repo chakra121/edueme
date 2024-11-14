78e533760dd5fb83d85ab51011f1723a212fa3e2
eduemeweb <edumeintern@gmail.com>
2024-11-14 19:15:31 +0530

    Learning Outcome
    Added matter in Learning outcome with some effects @chakra121

--- src/app/outcome/page.tsx
+++ src/app/outcome/page.tsx
@@ -4,7 +4,7 @@ const Outcome = () => {
   return (
     <div className="relative flex flex-col items-center px-32 py-20">
       {/* box */}
-      <div className="rounded-xl bg-neutral-600 px-10">
+      <div className="rounded-xl bg-neutral-600 px-9 py-2 hover:bg-stone-500">
         {/* items in box */}
         <div className="flex flex-col items-center p-8">
           <h1 className="p-4 text-3xl font-bold">
@@ -34,20 +34,119 @@ const Outcome = () => {
         </div>
       </div>
       {/* Grid */}
-      <div className="grid grid-cols-3 gap-4 p-4">
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 1</div>
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 2</div>
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 3</div>
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 4</div>
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 5</div>
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 6</div>
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 7</div>
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 8</div>
-    <div className="bg-neutral-500 font-semibold p-4 rounded">Text Item 9</div>
-</div>
-
+      <div className="grid grid-cols-3 gap-4 p-5">
+        {/* Box-1 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">
+            Many other advantages include:
+          </h1>
+          <p className="text-center font-normal">
+            - Improved creative thinking skills
+            <br /> - Improved social skills
+            <br /> - Improved problem-solving skills
+            <br /> - Improved computer programming skills
+          </p>
+        </div>
+        {/* Box-2 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">Build Confidence:</h1>
+          <p className="text-center font-normal">
+            Robotics programs for kids are beneficial because they help to build
+            confidence. Kids learn to be confident in their ideas and bring them
+            to light, sometimes literally. They realize they can make things
+            that other people can use. This makes them feel confident that they
+            are positively contributing to the world around them.
+          </p>
+        </div>
+        {/* Box-3 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">
+            Encourage Perseverance:
+          </h1>
+          <p className="text-center font-normal">
+            Children also learn perseverance during robotics programs. Projects
+            very rarely come out right the first time. This is how kids learn to
+            keep going and never give up, no matter how hard it may seem.
+          </p>
+        </div>
+        {/* Box-4 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">Accept Criticism:</h1>
+          <p className="text-center font-normal">
+            Another benefit of robotics programs is learning how to accept
+            constructive criticism. It can be difficult for people of all ages
+            to hear that their idea may not be the best or that they need to fix
+            something. But, these pieces of advice can help us all succeed.
+            Children learn how to use these pieces of criticism to their
+            advantage to improve their projects.
+          </p>
+        </div>
+        {/* Box-5 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">
+            Improves Engagement:
+          </h1>
+          <p className="text-center font-normal">
+            Robotics encourages hands-on learning. It can help to improve
+            concentration and attention levels which can make kids more engaged
+            in the classroom. Robotics for kids can also help to improve
+            concentration.
+          </p>
+        </div>
+        {/* Box-6 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">
+            Improves Programming Skills:
+          </h1>
+          <p className="text-center font-normal">
+            Robotics for kids helps to introduce the basics of programming to
+            kids. Since robotics is hands-on, children usually find it
+            interesting.
+          </p>
+        </div>
+        {/* Box-7 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">
+            Encourages Teamwork:
+          </h1>
+          <p className="text-center font-normal">
+            When children take part in robotics, they also learn how to work
+            together. Often, kids work together on projects and learn how to
+            socialize and bounce ideas off of one another. This is a skill they
+            can use their entire lives.
+          </p>
+        </div>
+        {/* Box-8 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">
+            Preparing for the Future:
+          </h1>
+          <p className="text-center font-normal">
+            Robotics for kids is also important because it is the wave of the
+            future. If children can get on board now, they will be better off as
+            they get older. Recent statistics indicated that the robotics job
+            outlook was expected to grow an average of four percent between 2018
+            and 2028. Giving children the skills they need now to compete in
+            that job market as they get older is of the utmost importance.
+          </p>
+        </div>
+        {/* Box-9 */}
+        <div className="transform rounded border border-green-900 bg-neutral-500 p-4 font-semibold transition duration-500 ease-in-out hover:z-10 hover:-translate-y-1 hover:scale-125 hover:bg-amber-500 hover:text-black">
+          <h1 className="text-center text-lg font-bold">
+            At What Age Should I Introduce My Child to Robotics?
+          </h1>
+          <p className="text-center font-normal">
+            The answer will differ depending on your child because children
+            develop at different stages. Some children may be ready for robotics
+            for beginners at age 6 while others would be best suited to start at
+            age 8 or 9. You know your child best. If you think a class for
+            6-year-olds would be age-appropriate,try it. If not, you can wait
+            until they’re a couple of years older.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Outcome
+export default Outcome;
