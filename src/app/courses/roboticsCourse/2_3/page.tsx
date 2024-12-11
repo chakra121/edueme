e26bca7aff86d63b048c65edef4c686552b777bf
eduemeweb <edumeintern@gmail.com>
2024-12-11 22:31:44 +0530

    Update page.tsx

--- src/app/courses/roboticsCourse/2_3/page.tsx
+++ src/app/courses/roboticsCourse/2_3/page.tsx
@@ -4,9 +4,20 @@ import CourseContent from "./courseContent";
 
 const page = () => {
   return (
-    <div>
+    <div className="relative">
+      <div className="pt-[10%] grid grid-cols-3">
+        <div></div>
+        <div className="fixed top-20 z-20 rounded-lg bg-red-600 col-span-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nam
+          neque natus iste beatae facere, enim magni. Voluptatibus labore
+          provident laboriosam vel vero quidem mollitia natus. Adipisci cum
+          dicta quae?
+        </div>
+      </div>
+      <div className="relative">
         <Brief />
         <CourseContent />
+      </div>
     </div>
   );
 };
