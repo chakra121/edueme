6ea921033da8b8e1e84b2277866d01b095f17f2b
eduemeweb <edumeintern@gmail.com>
2024-12-23 00:32:32 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Dashboard from './dashboard'
-import Sidepanel from "@/components/Sidepanel";
+// import Sidepanel from "@/components/Sidepanel";
 
 const page = () => {
   return (
