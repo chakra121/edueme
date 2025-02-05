9bde3a63af8f13e55663ff57bdcf0a4ad784a88e
Rohit2764 <138191904+Rohit2764@users.noreply.github.com>
2025-02-05 10:24:36 +0530

    ...

--- src/app/dashboard/teacherDashboard/dclassdetails/dclassdetails.tsx
+++ src/app/dashboard/teacherDashboard/dclassdetails/dclassdetails.tsx
@@ -170,7 +170,7 @@ const Dashboard = () => {
         </div>
 
         {isModalOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 ">
             <div className="bg-white rounded-xl p-6 w-full max-w-md">
               <h2 className="text-2xl font-bold mb-4">
                 {editingSession ? 'Edit Class' : 'New Class'}
