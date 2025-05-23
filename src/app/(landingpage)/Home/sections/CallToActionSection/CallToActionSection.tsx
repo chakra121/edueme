5a5deca59ed945297845b4b0d586c2cda00c71da
Sanjay-Batthula <sanjaybatthula24@gmail.com>
2025-05-23 13:24:38 +0530

    Update CallToActionSection.tsx

--- src/app/(landingpage)/Home/sections/CallToActionSection/CallToActionSection.tsx
+++ src/app/(landingpage)/Home/sections/CallToActionSection/CallToActionSection.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
+const partnerLogos = [
+  { src: "/nuroscience.jpg", alt: "Product - Neuroscience" },
+  { src: "/ai_robotics.jpg", alt: "Product - AI Robotics" },
+  { src: "/young_innovators.jpg", alt: "Product - Young Innovators" },
+  { src: "/mech_logo.jpg", alt: "Mechatronites_Club" }
+];
 
 export const CallToActionSection = (): JSX.Element => {
   return (
@@ -18,63 +25,50 @@ export const CallToActionSection = (): JSX.Element => {
                 to enhance education and streamline learning experiences for
                 students, educators, and institutions.
               </p>
-              <Button className="bg-black text-white rounded-[14px] px-[35px] py-5 h-auto">
-                <span className="font-space-grotesk font-normal text-xl leading-7">
-                  Get Your Demo Now
-                </span>
-              </Button>
             </div>
 
-            <div className="relative w-full lg:w-[494px] h-[394px]">
-              <div className="absolute w-[359px] h-[394px] top-0 left-0">
-                <div className="absolute w-[359px] h-[394px] top-0 left-0 rotate-180">
-                  <div className="relative h-[394px]">
-                    <div className="absolute w-[338px] h-[71px] top-40 left-[21px] rounded-[169px/35.5px] border border-solid border-[#000000] rotate-180" />
-                    <div className="absolute w-[338px] h-[71px] top-[135px] left-[21px] rounded-[169px/35.5px] border border-solid border-[#000000] rotate-180" />
-                    <div className="absolute w-[338px] h-[71px] top-[110px] left-[21px] rounded-[169px/35.5px] border border-solid border-[#000000] rotate-180" />
-                    <Image
-                    width={199}
-                    height={209}
-                      className="absolute w-[199px] h-[209px] top-[185px] left-[19px] -rotate-180"
-                      alt="Star"
-                      src="/prodcuts/star-2.svg"
-                    />
-                    <div className="absolute w-[164px] h-[164px] top-[53px] left-0">
-                      <Image
-                      width={160}
-                      height={164}
-                        className="absolute w-[160px] h-[156px] top-0 left-0.5"
-                        alt="Star"
-                        src="/prodcuts/star-4.svg"
-                      />
-                    </div>
-                    <Image
-                      width={130}
-                      height={130}
-                      className="absolute w-[130px] h-[130px] top-0 left-[159px] -rotate-180"
-                      alt="Group"
-                      src="/prodcuts/group-2.png"
-                    />
-                    <div className="absolute w-[125px] h-[125px] top-[145px] left-36 bg-[#000000] rounded-[62.5px] border border-solid rotate-180" />
-                    <div className="absolute w-5 h-10 top-[201px] left-[217px] bg-white rounded-[10px/20px] rotate-180" />
-                    <div className="absolute w-5 h-10 top-[201px] left-44 bg-white rounded-[10px/20px] rotate-180" />
-                  </div>
+            <div className="relative w-full lg:w-[494px] h-[394px] overflow-hidden">
+              <div className="absolute inset-0 flex items-center justify-center">
+                <div className="relative w-[300px] h-[300px] bg-gradient-to-b from-gray-50 to-transparent rounded-full">
+                  <motion.div
+                    animate={{
+                      y: [0, -400, 0],
+                    }}
+                    transition={{
+                      duration: 20,
+                      repeat: Infinity,
+                      ease: "linear"
+                    }}
+                    className="flex flex-col gap-8 items-center"
+                  >
+                    {partnerLogos.map((logo, index) => (
+                      <motion.div
+                        key={index}
+                        className="w-[120px] h-[120px] bg-white rounded-xl shadow-lg p-4 flex items-center justify-center"
+                        whileHover={{ scale: 1.05 }}
+                      >
+                        <Image
+                          src={logo.src}
+                          alt={logo.alt}
+                          width={100}
+                          height={100}
+                          className="object-contain"
+                        />
+                      </motion.div>
+                    ))}
+                  </motion.div>
                 </div>
-                <Image
-                  width={74}
-                  height={74}
-                  className="absolute w-[74px] h-[74px] top-[68px] left-[203px] object-cover"
-                  alt="Image"
-                  src="/prodcuts/image-11.png"
-                />
-              </div>
-              <div className="absolute w-[188px] h-[202px] top-[62px] left-[-198px] bg-[url(/star-2.svg)] bg-[100%_100%]">
-                <Image
-                  width={89}
-                  height={85}
-                  className="absolute w-[89px] h-[85px] top-[58px] left-[49px] object-cover"
-                  alt="Image"
-                  src="/prodcuts/image.png"
+                
+                <motion.div
+                  animate={{
+                    rotate: 360,
+                  }}
+                  transition={{
+                    duration: 20,
+                    repeat: Infinity,
+                    ease: "linear"
+                  }}
+                  className="absolute w-full h-full border-2 border-dashed border-gray-200 rounded-full"
                 />
               </div>
             </div>
